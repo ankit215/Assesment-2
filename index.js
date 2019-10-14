6e314e806b80be093d714e742af104f485db8a04
Ankit Rawat <55525947+ankit215@users.noreply.github.com>
2019-10-14 14:28:14 +0530

    crud

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const mysql = require('mysql');
 const express = require('express');
 var app = express();
-const bodyparser = require(body-parser);
+const bodyparser = require('body-parser');
+app.use(bodyparser.json()); 
 
-app.get(bodyparser.json()); 
-
-var mysqlConnection= mysql.createConnection({
+var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234'
-    database: 'Office'
+    password: '',
+    database: 'Employee',
     multipleStatement : true
 });
 
 mysqlConnection.connect((err)=>{
     if(!err)
-    console.log("DB connection succeded.")
-    else
-    console.log("DB connection failed \n Error : " + JSON.stringify(err,undefined,  2))
-})
+    console.log("DB connection succeded.");
+      else
+    console.log("DB connection failed \n Error : " + JSON.stringify(err,undefined,  2));
+});
 
-app.listen(3000,()=>console.log('Express server is running at port no: 3000'));
+app.listen(3002,()=>console.log('Express server is running at port no: 6000'));
+
 // Get all employess
-app.get('/employes',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM Employee',(err, rows, feild)=>{
-        if (err)
+app.get('/employees',(req,res)=>{
+    mysqlConnection.query('SELECT * FROM employee',(err, rows, feilds )=>{
+        if (!err)
         res.send(rows);
         else
         console.log(err)
@@ -32,33 +32,34 @@ app.get('/employes',(req,res)=>{
 })
 //get an employee
 
-app.get('/employes/:id',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM Employee WHERE EmpID = ?',[req.params.id](err, rows, feild)=>{
-        if (err)
+app.get('/employees/:id',(req,res)=>{
+    mysqlConnection.query('SELECT * FROM employee WHERE EmpID = ?',[req.params.id],(err, rows, feild)=>{
+        if (!err)
         res.send(rows);
-        else                                                                                                                                                                                                                                                                          
-        console.log(err)
+        else                                                                                                                                                                                                                                                                     
+        console.log(err);
     })
 });
 
 //delete an employess
-app.get('/employes/:id',(req,res)=>{
-    mysqlConnection.query('DELETE FROM Employee WHERE EmpID = ?',[req.params.id](err, rows, feild)=>{                                                                                                                                                                                                                                                                                                                                                                                                           * FROM Employee WHERE EmpID = ?',[req.params.id](err, rows, feild)=>{
+app.delete('/employees/:id',(req,res)=>{
+    mysqlConnection.query('DELETE FROM employee WHERE EmpID = ?',[req.params.id], (err, rows, feilds )=>{                                                                                                                                                                                                                                                                                                                                                                                   
         if (!err)
-        console.log("Deleted Sucessfully.")
+        res.send("Deleted Sucessfully.");
         else
-        console.log(err)
+        console.log(err)    
     })
 });
-
 //insert an employess
-app.post('/employes',(req,res)=>{
+app.put('/employees',(req,res)=>{
+    let emp= req.body;
     var sql = "SET @EmpID=? ;SET @Name =? ; SET @EmpCode=?; SET @Salary=?;\
-    Call EmployeeAddOrEdit(@EmpID=?,@Name =?, @EmpCode=?,@Salary=?)"
-    mysqlConnection.query(sql,[emp.EmpID,emp.Name,emp.EmpCode,empSalary],[req.params.id](err, rows, feild)=>{                                                                                                                                                                                                                                                                                                                                                                                                           * FROM Employee WHERE EmpID = ?',[req.params.id](err, rows, feild)=>{
-        if (!err)
-        res.send(rows);
+    Call EmployeeAddOrEdit(@EmpID,@Name, @EmpCode,@Salary)"
+    mysqlConnection.query(sql,[emp.EmpID,emp.Name,emp.EmpCode,empSalary],[req.params.id],(err, rows, feild)=>{       
+    if (!err)
+        res.send("Updated Succesfully.")
         else
         console.log(err)
+    
     })
-});
+})
